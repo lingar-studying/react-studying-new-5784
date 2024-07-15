e019c02a48351ee249d9e5cb4dc0d654dffbcd86
lingar <yimprogramming@gmail.com>
2024-07-16 00:38:49 +0300

    Removing old

--- src/App.js
+++ src/App.js
@@ -13,50 +13,60 @@ const App = () => {
   const quillRef = useRef();
 
   return (
+      <>
       <div>
-        <Editor
-            ref={quillRef}
-            readOnly={readOnly}
-            defaultValue={new Delta()
-                .insert('Hello')
-                .insert('\n', { header: 1 })
-                .insert('Some ')
-                .insert('initial', { bold: true })
-                .insert(' ')
-                .insert('content', { underline: true })
-                .insert('\n')}
-            onSelectionChange={setRange}
-            onTextChange={setLastChange}
-        />
-        <div class="controls">
-          <label>
-            Read Only:{' '}
-            <input
-                type="checkbox"
-                value={readOnly}
-                onChange={(e) => setReadOnly(e.target.checked)}
-            />
-          </label>
-          <button
-              className="controls-right"
-              type="button"
-              onClick={() => {
-                alert(quillRef.current?.getLength());
-              }}
-          >
-            Get Content Length
-          </button>
-        </div>
-        <div className="state">
-          <div className="state-title">Current Range:</div>
-          {range ? JSON.stringify(range) : 'Empty'}
-        </div>
-        <div className="state">
-          <div className="state-title">Last Change:</div>
-          {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
-        </div>
+          <h1>Hi</h1>
+
       </div>
+      </>
   );
+
+
+
+    //Quil - not working. TODO -organize
+      // <div>
+      //   <Editor
+      //       ref={quillRef}
+      //       readOnly={readOnly}
+      //       defaultValue={new Delta()
+      //           .insert('Hello')
+      //           .insert('\n', { header: 1 })
+      //           .insert('Some ')
+      //           .insert('initial', { bold: true })
+      //           .insert(' ')
+      //           .insert('content', { underline: true })
+      //           .insert('\n')}
+      //       onSelectionChange={setRange}
+      //       onTextChange={setLastChange}
+      //   />
+      //   <div class="controls">
+      //     <label>
+      //       Read Only:{' '}
+      //       <input
+      //           type="checkbox"
+      //           value={readOnly}
+      //           onChange={(e) => setReadOnly(e.target.checked)}
+      //       />
+      //     </label>
+      //     <button
+      //         className="controls-right"
+      //         type="button"
+      //         onClick={() => {
+      //           alert(quillRef.current?.getLength());
+      //         }}
+      //     >
+      //       Get Content Length
+      //     </button>
+      //   </div>
+      //   <div className="state">
+      //     <div className="state-title">Current Range:</div>
+      //     {range ? JSON.stringify(range) : 'Empty'}
+      //   </div>
+      //   <div className="state">
+      //     <div className="state-title">Last Change:</div>
+      //     {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
+      //   </div>
+      // </div>
 };
 
 export default App;
