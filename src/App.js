041d20699dd6e20e2aaff60eae25a24d881d29a2
Oshir <osherp96@gmail.com>
2025-12-17 20:04:17 +0200

    osher new list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 export default function App() {
   return (
+    <>
     <div>
       <h1>Test</h1>
-      <p>accept</p>
+      <p className='pforcheckbox'>accept</p>
       <input  type="checkbox"/>
     </div>
+    <div>
+      <h1 className='osher_class'>Hello World!</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+    </>
   );
 }
