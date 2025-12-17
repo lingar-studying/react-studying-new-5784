1cc3e8d1355a3ba207d7d181d664c77e53a1de04
shlomohal84 <stairwaytoheaven84@gmail.com>
2025-12-17 19:39:36 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-studying-new-5784
+
 Learning and demonstrating things about react in the first version. Since 5784 in Hebrew year, 2024. (Tamuz)
+
+#test
--- src/App.js
+++ src/App.js
@@ -1,112 +1,71 @@
-import React, { useRef, useState } from 'react';
-//import Editor from './Editor';
-//import Quill from "quill";
-import CkEditorDemo from './CkEditorDemo';
-import {Box, Button, Link, TextField, Typography} from "@mui/material";
-import { DataGrid } from '@mui/x-data-grid';
+// //import Editor from './Editor';
+// //import Quill from "quill";
+// import CkEditorDemo from "./CkEditorDemo";
+// import { Box, Button, Link, TextField, Typography } from "@mui/material";
+// import { DataGrid } from "@mui/x-data-grid";
 
-import ElementsDrawerComponent from "./ElementsDrawerComponent";
-import DataGridPoc from "./DataGridPoc";
-// const Delta = Quill.import('delta');
+// import ElementsDrawerComponent from "./ElementsDrawerComponent";
+// import DataGridPoc from "./DataGridPoc";
+// // const Delta = Quill.import('delta');
 
-const App = () => {
-  const [range, setRange] = useState();
-  const [lastChange, setLastChange] = useState();
-  const [readOnly, setReadOnly] = useState(false);
+export default function App() {
+  //   const [range, setRange] = useState();
+  //   const [lastChange, setLastChange] = useState();
+  //   const [readOnly, setReadOnly] = useState(false);
 
-  // Use a ref to access the quill instance directly
-  const quillRef = useRef();
+  //   // Use a ref to access the quill instance directly
+  //   const quillRef = useRef();
 
   return (
-      <>
-      <div>
-          <Typography component={"div"} sx={{padding: "5rem"}}>
-
-              <h1 >Stampli task</h1>
-            <ElementsDrawerComponent/>
-
-              <h1>Data grid</h1>
-              <DataGridPoc/>
-
-
-
-
-
-
-          </Typography>
-
-
-
-
-
-          {/*<Box component={"h1"}>U need to wrap it inside Typography for get MUI environment</Box>*/}
-
-          {/*<Typography variant="h1" gutterBottom>*/}
-
-          {/*    H1 MUI?22*/}
-          {/*</Typography>*/}
-
-          {/*<Typography>*/}
-          {/*    <Link href = "https://mui.com/material-ui/getting-started/"*/}
-          {/*          target={"_blank"}*/}
-          {/*    >MUI DOCS</Link>*/}
-          {/*  <p>General Typography</p>*/}
-          {/*    <h2>H2</h2>*/}
-          {/*    <h1>h1 here</h1>*/}
-
-          {/*</Typography>*/}
-          {/*<Button variant="contained">Hello world</Button>*/}
-
-      </div>
-      </>
+    <div>
+      <h1>Test</h1>
+    </div>
   );
+}
+//Quil - not working. TODO -organize
+// <div>
+//   <Editor
+//       ref={quillRef}
+//       readOnly={readOnly}
+//       defaultValue={new Delta()
+//           .insert('Hello')
+//           .insert('\n', { header: 1 })
+//           .insert('Some ')
+//           .insert('initial', { bold: true })
+//           .insert(' ')
+//           .insert('content', { underline: true })
+//           .insert('\n')}
+//       onSelectionChange={setRange}
+//       onTextChange={setLastChange}
+//   />
+//   <div class="controls">
+//     <label>
+//       Read Only:{' '}
+//       <input
+//           type="checkbox"
+//           value={readOnly}
+//           onChange={(e) => setReadOnly(e.target.checked)}
+//       />
+//     </label>
+//     <button
+//         className="controls-right"
+//         type="button"
+//         onClick={() => {
+//           alert(quillRef.current?.getLength());
+//         }}
+//     >
+//       Get Content Length
+//     </button>
+//   </div>
+//   <div className="state">
+//     <div className="state-title">Current Range:</div>
+//     {range ? JSON.stringify(range) : 'Empty'}
+//   </div>
+//   <div className="state">
+//     <div className="state-title">Last Change:</div>
+//     {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
+//   </div>
+// </div>
+// };
 
-
-
-    //Quil - not working. TODO -organize
-      // <div>
-      //   <Editor
-      //       ref={quillRef}
-      //       readOnly={readOnly}
-      //       defaultValue={new Delta()
-      //           .insert('Hello')
-      //           .insert('\n', { header: 1 })
-      //           .insert('Some ')
-      //           .insert('initial', { bold: true })
-      //           .insert(' ')
-      //           .insert('content', { underline: true })
-      //           .insert('\n')}
-      //       onSelectionChange={setRange}
-      //       onTextChange={setLastChange}
-      //   />
-      //   <div class="controls">
-      //     <label>
-      //       Read Only:{' '}
-      //       <input
-      //           type="checkbox"
-      //           value={readOnly}
-      //           onChange={(e) => setReadOnly(e.target.checked)}
-      //       />
-      //     </label>
-      //     <button
-      //         className="controls-right"
-      //         type="button"
-      //         onClick={() => {
-      //           alert(quillRef.current?.getLength());
-      //         }}
-      //     >
-      //       Get Content Length
-      //     </button>
-      //   </div>
-      //   <div className="state">
-      //     <div className="state-title">Current Range:</div>
-      //     {range ? JSON.stringify(range) : 'Empty'}
-      //   </div>
-      //   <div className="state">
-      //     <div className="state-title">Last Change:</div>
-      //     {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
-      //   </div>
-      // </div>
-};
-
-export default App;
+// export default App;
