bb87d0a628ece8aa53c055fa4c4a3151392a3102
moran amar <moran@moran.com>
2025-12-17 21:27:04 +0200

    Moran

--- src/input.jsx
+++ src/input.jsx
@@ -5,7 +5,7 @@ function MessageTemplates() {
   <span style={{ color: "blue" }}>שלום, האם המוצר עדיין זמין?</span>,
   <span style={{ color: "yellow" }}>שלום, האם המוצר עדיין זמין?</span>,
   <span style={{ color: "green" }}>שלום, האם המוצר עדיין זמין?</span>,
-  <span style={{ color: "green" }}>שלום, מה שווי הערך ?</span>,
+  <span style={{ color: "orange" }}>שלום, מה שווי הערך ?</span>,
   "מה המחיר הסופי שאתה מבקש?"
 ];
 // בזמן הרינדור:
